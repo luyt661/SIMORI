1d9e93bd31a81056a0bfc7d27acc0ece2965520b
luyt661 <luytpv1703@gmail.com>
2026-06-11 11:24:34 +0700

    home

--- SHIMORI_FE/src/App.jsx
+++ SHIMORI_FE/src/App.jsx
@@ -19,7 +19,8 @@ function App() {
         }
       >
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Home />} />
+
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
 
